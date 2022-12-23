325f62d9f744c20831e4aacee1e1f9b1a724811a
Micah Newsum <micah.newsum@glidewelldental.com>
2022-12-23 08:43:24 -0800

    Add trailing period and reformat.

--- week1/HelloGoodbye.java
+++ week1/HelloGoodbye.java
@@ -3,7 +3,7 @@ public class HelloGoodbye {
         String firstName = args[0];
         String secondName = args[1];
 
-        System.out.println("Hello "+firstName+" and "+secondName);
-        System.out.println("Goodbye "+secondName+" and "+firstName);
+        System.out.println("Hello " + firstName + " and " + secondName + ".");
+        System.out.println("Goodbye " + secondName + " and " + firstName + ".");
     }
 }
